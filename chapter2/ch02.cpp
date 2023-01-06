bc2bcb6337ce21bc5c9b919a7e78eba44ac26069
litongxi <1962350369@qq.com>
2023-01-06 15:27:11 +0800

    chap02

---
+++ chapter2/ch02.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+void q_2_3()
+{
+    unsigned u1 = 10, u2 = 42;
+    // 32
+    std::cout << u2 - u1 << std::endl;
+    // 2^32
+    std::cout << u1 - u2 << std::endl;
+
+    int i1 = 10, i2 = 42;
+    // 32 -32
+    std::cout << i2 - i1 << std::endl;
+    std::cout << i1 - i2 << std::endl;
+    // 0
+    std::cout << i1 - u1 << std::endl;
+    std::cout << u1 - i1 << std::endl;
+}
+
+void q_2_8()
+{
+    std::cout << 2 << "\x4d\12" << std::endl;
+    std::cout << 2 << "\t\115\12" << std::endl;
+}
+
+void q_2_17()
+{
+    int i = 12, &ref_i = i;
+    double d = 13, &ref_d = d;
+    ref_i = d;
+    std::cout << i << " " << ref_i << std::endl;
+    ref_d = 20;
+    std::cout << d << " " << ref_d << std::endl;
+}
+
+void q_2_18()
+{
+    int val = 1024, b = 1;
+    int *pval = &val, *p2 = pval;
+    std::cout << *pval << " " << pval << std::endl;
+    // change the value of a pointer
+    pval = &b;
+    std::cout << *pval << " " << pval << " " << val << std::endl;
+    // change the value to which the pointer points
+    *p2 = b;
+    std::cout << *pval << " " << val << std::endl;
+}
+
+void q_2_20()
+{
+    const int v2 = 1024;
+    int v1 = v2;
+    int *p1 = &v1, &r1 = v1;
+    const int *p2 = &v2, *const p3 = &v1, &r2 = v2;
+
+    std::cout << *p2 << " " << p2 << " " << p1 << std::endl;
+    std::cout << *p3 << " " << p3 << " " << r2 << std::endl;
+    p2 = p1;
+    std::cout << *p2 << " " << p2 << " " << p1 << std::endl;
+    p2 = p3;
+    std::cout << *p2 << " " << p2 << " " << p1 << std::endl;
+}
+
+int main()
+{
+    // q_2_3();
+    // q_2_8();
+    // q_2_17();
+    q_2_18();
+    q_2_20();
+    return 0;
+}
